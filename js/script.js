a5dcb0213addf0ec1d9060be46f332a96893ea4e
Christian <Christian.Spelt@student.rocva.nl>
2021-12-08 11:09:33 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+function home() {
+  location.replace("index.html");
+}
+function wie() {
+  location.replace("wie.html");
+}
+function pro() {
+  location.replace("pro.html");
+}
+function mod() {
+  location.replace("mod.html");
+}
+
+function self(){
+  alert("Ben je all!!")
+}
+function audio(){
+var audio = new Audio('audio/samsung.mp3');
+audio.play();
+}
+
+function github(){
+  location.replace("https://github.com/Christianspelt?tab=repositories");
+}
